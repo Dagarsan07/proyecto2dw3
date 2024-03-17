f201817cb395710a73f32bb6cc23bec22254c1be
David García Sánchez <garciasanchezdavid097@gmail.com>
2024-03-17 15:36:46 +0100

    Implementación PrimeVue + configuración + auto import de componentes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,14 @@ import "./bootstrap";
 import router from "./router";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 import "../css/app.css";
+
+import "primevue/resources/themes/aura-light-blue/theme.css";
 
 import App from "./App.vue";
 
+const app = createApp(App);
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+app.use(pinia).use(router).use(PrimeVue).mount("#app");
